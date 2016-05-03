7889f8fe8da5c7fc67a93b7399eddf2d7babc120
dnkllhr <dnkllhr@gmail.com>
2016-05-03 15:06:34 -0400

    A enter message text field added as well as a button

---
+++ app/src/main/java/com/example/aidan/database/DisplayMessageActivity.java
@@ -0,0 +1,22 @@
+package com.example.aidan.database;
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+    }
+
+}
