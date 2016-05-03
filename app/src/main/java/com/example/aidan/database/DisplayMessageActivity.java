0c9a51fe786783416c32cd7a89a9ba498ca47036
dnkllhr <dnkllhr@gmail.com>
2016-05-03 15:29:47 -0400

    push to master

--- app/src/main/java/com/example/aidan/database/DisplayMessageActivity.java
+++ app/src/main/java/com/example/aidan/database/DisplayMessageActivity.java
@@ -23,7 +23,7 @@ public class DisplayMessageActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "No action here yet", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
